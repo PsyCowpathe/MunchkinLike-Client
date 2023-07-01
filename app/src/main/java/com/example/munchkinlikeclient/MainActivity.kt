6eea63a6b1dd38d82204d3efff329c6a8d68d224
Agirona <agirona@student.42lyon.fr>
2023-07-01 02:09:37 +0200

    The user now need to change his name at first connection

--- app/src/main/java/com/example/munchkinlikeclient/MainActivity.kt
+++ app/src/main/java/com/example/munchkinlikeclient/MainActivity.kt
@@ -1,54 +1,89 @@
 package com.example.munchkinlikeclient
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.text.Editable
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.ui.AppBarConfiguration
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.munchkinlikeclient.databinding.ActivityMainBinding
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity()
 {
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
     private lateinit var dataStore: DataStore<Preferences>
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
-
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main);
         dataStore = createDataStore(name = "settings")
         findViewById<Button>(R.id.LogButton).setOnClickListener{
             lifecycleScope.launch {
                 sendLoginRequest();
-                showProfile();
+                makeRegister();
             }
         }
     }
 
-    private fun showProfile()
+    private fun makeRegister()
     {
-        setContentView(binding.root)
+        setContentView(R.layout.name_form)
+        val form = findViewById<EditText>(R.id.NameForm);
+
+        findViewById<Button>(R.id.SubmitButton).setOnClickListener {
+            lifecycleScope.launch {
+                sendRegisterRequest(form.text);
+            }
+        }
+    }
+
+    private suspend fun sendRegisterRequest(name : Editable)
+    {
+        val queue = Volley.newRequestQueue(this)
+        var token: String = read("token") ?: "";
+
+        val stringRequest = object : StringRequest(Request.Method.POST,
+            getString(R.string.back_url) + "register",
+            Response.Listener
+            { response ->
+                val responseJson = JSONObject(response)
+                if (!responseJson.isNull("token"))
+                    lifecycleScope.launch{
+                        save("token", responseJson.get("token").toString())
+                    }
+            },
+            { response ->
+                println(response);
+            })
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): MutableMap<String, String>
+            {
+                val headers = HashMap<String, String>();
+                headers["Authorization"] = token ?: "";
+                return headers;
+            }
+
+            override fun getParams(): Map<String, String>
+            {
+                val params: MutableMap<String, String> = HashMap()
+                params["name"] = name.toString();
+                return params
+            }
+        }
+        queue.add(stringRequest)
     }
 
     private suspend fun sendLoginRequest()
